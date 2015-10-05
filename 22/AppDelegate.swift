992e524e4d998e6fa60a2abd5da77b470da12d82
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-10-05 17:20:57 +0200

    Add basic project.

---
+++ 22/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  22
+//
+//  Created by Jelle Vandenbeeck on 05/10/15.
+//  Copyright © 2015 Fousa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+

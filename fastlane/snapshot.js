58a21eaad5872aa56049e9521f93e4413e2697e0
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-10-07 22:17:03 +0200

    Update fastlane.

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -1,9 +1,22 @@
 #import "SnapshotHelper.js"
 
 var target = UIATarget.localTarget();
-var app = target.frontMostApp();
-var window = app.mainWindow();
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
 
+// Show the information screen.
+captureLocalizedScreenshot("1-Information")
+target.frontMostApp().mainWindow().buttons()["Set your birthdate"].tap();
 
-target.delay(3)
-captureLocalizedScreenshot("0-LandingScreen")
+// Select my birthday.
+target.frontMostApp().mainWindow().pickers()[0].wheels()[0].selectValue("June");
+target.frontMostApp().mainWindow().pickers()[0].wheels()[1].selectValue("28");
+target.frontMostApp().mainWindow().pickers()[0].wheels()[2].selectValue("1984");
+captureLocalizedScreenshot("2-Birthdate")
+
+// Show the counter screen.
+target.frontMostApp().mainWindow().buttons()["Finish"].tap();
+captureLocalizedScreenshot("0-Counter")
+
+// Show the settings screen.
+target.frontMostApp().mainWindow().buttons()["Settings"].tap();
+captureLocalizedScreenshot("3-Settings")

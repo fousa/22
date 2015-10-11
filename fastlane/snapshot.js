baecd24e02f482bbf1f4f7dbc2ce59dde9e8c841
Jelle Vandebeeck <jelle@fousa.be>
2015-10-11 17:22:12 +0200

    Update snapshot.

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -5,18 +5,21 @@ target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
 
 // Show the information screen.
 captureLocalizedScreenshot("1-Information")
-target.frontMostApp().mainWindow().buttons()["Set your birthdate"].tap();
+target.frontMostApp().mainWindow().buttons()["SET YOUR BIRTH YEAR"].tap();
 
 // Select my birthday.
-target.frontMostApp().mainWindow().pickers()[0].wheels()[0].selectValue("June");
-target.frontMostApp().mainWindow().pickers()[0].wheels()[1].selectValue("28");
-target.frontMostApp().mainWindow().pickers()[0].wheels()[2].selectValue("1984");
+target.frontMostApp().mainWindow().pickers()[0].wheels()[0].selectValue("1984");
 captureLocalizedScreenshot("2-Birthdate")
 
 // Show the counter screen.
-target.frontMostApp().mainWindow().buttons()["Finish"].tap();
+target.frontMostApp().mainWindow().buttons()["CALCULATE AGE"].tap();
 captureLocalizedScreenshot("0-Counter")
 
 // Show the settings screen.
-target.frontMostApp().mainWindow().buttons()["Settings"].tap();
+target.frontMostApp().mainWindow().buttons()["Open settings"].tap();
+target.frontMostApp().mainWindow().pickers()[0].wheels()[0].selectValue("1990");
 captureLocalizedScreenshot("3-Settings")
+
+// Show the settings screen.
+target.frontMostApp().mainWindow().buttons()["Close settings"].tap();
+captureLocalizedScreenshot("4-Counter")
